a543f04a9bfcef900a05d8673461be33e7ae0f6f
Jhurgen Maraza <JhurgenMrz99@gmail.com>
2019-10-16 13:48:28 -0300

    Update Page House-data and add loader

--- AccessAndDisplayPublicData/model/congress-data.js
+++ AccessAndDisplayPublicData/model/congress-data.js
@@ -1,43 +1,12 @@
-const tableHouse = document.getElementById('house-data')
+var house_data = new Vue({
+    el:'#house-data',
+    data: {
+        loading: true,
+        members: []
+    }
+})
 
-function renderTableHouse(data){
-
-    const header = `<thead>
-            <tr class="text-center">
-                <th>Members</th>
-                <th>First Name</th>
-                <th>Middle Name</th>
-                <th>Last Name</th>
-                <th>Party</th>
-                <th> Total Votes </th>
-            </tr>
-        </thead>
-    `
-    const rows = []
-    rows.push(data.map((members,index)=>{
-        if(!members.middle_name){
-            members.middle_name = '||'
-        }
-        return `<tr>
-            <td> #${index+1}</td>
-            <td> <a href=${members.url}>${members.first_name}</a> </td> 
-            <td> <a href=${members.url}>${members.middle_name}</a> </td> 
-            <td> <a href=${members.url}>${members.last_name}</a> </td> 
-            <td> ${members.party}</td>
-            <td> ${members.total_votes} </td>   
-        </tr>
-        `
-    }).join(''))
-    // console.log(rows)
-
-    const Table =  `<thead class="text-center">
-        ${header}    
-    </thead>
-    <tbody class="text-center">
-        ${rows}
-    </tbody>
-    `
-    return Table
-}
-
-tableHouse.innerHTML = renderTableHouse(data.results[0].members)
+FetchData('house').then(data => {
+    house_data.loading = false
+    house_data.members = data
+})
--- AccessAndDisplayPublicData/model/senate-data.js
+++ AccessAndDisplayPublicData/model/senate-data.js
@@ -1,11 +1,13 @@
 var senate_data = new Vue({
     el: '#senate-data',
     data:{
+        loading: true,
         members: []
     }
 })
 
 FetchData('senate').then( data =>{
+    senate_data.loading =  false
     senate_data.members = data
 })
 .catch(error => error)
